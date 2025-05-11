8d04b6d68a5ec3a3b4007ac237e6030ab3b24dc3
Thiên Phúc <20127681@student.hcmus.edu.vn>
2025-05-11 16:22:40 +0700

    requirements edit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cellSegmentation",
+    version="0.0.0",
+    author="Phuc Nguyen",
+    packages=find_packages(),
+    install_requires = []
+)
