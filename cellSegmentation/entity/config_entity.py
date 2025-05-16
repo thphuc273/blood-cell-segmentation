c55d04a7448fc42a0e29462257148c0260311dc4
Thiên Phúc <20127681@student.hcmus.edu.vn>
2025-05-16 22:04:56 +0700

    update number of epochs

--- cellSegmentation/entity/config_entity.py
+++ cellSegmentation/entity/config_entity.py
@@ -16,7 +16,7 @@ DATA_VALIDATION_ALL_REQUIRED_FILE = ["train", "validation", "test", "data.yaml"]
 
 MODEL_TRAINER_DIR_NAME: str = "model_trainer"
 MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov9c-seg.pt"
-MODEL_TRAINER_EPOCHS: int = 1
+MODEL_TRAINER_EPOCHS: int = 40
 MODEL_TRAINER_BATCH_SIZE: int = 16
 
 @dataclass
