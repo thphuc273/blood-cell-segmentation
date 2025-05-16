ca80e5fa55a874d4e99cc571a45c9c018fe11e85
Thiên Phúc <20127681@student.hcmus.edu.vn>
2025-05-16 22:24:48 +0700

    edit yolo model

--- cellSegmentation/entity/config_entity.py
+++ cellSegmentation/entity/config_entity.py
@@ -15,7 +15,7 @@ DATA_VALIDATION_STATUS_FILE: str = "status.txt"
 DATA_VALIDATION_ALL_REQUIRED_FILE = ["train", "validation", "test", "data.yaml"]
 
 MODEL_TRAINER_DIR_NAME: str = "model_trainer"
-MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov9c-seg.pt"
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov8s-seg.pt"
 MODEL_TRAINER_EPOCHS: int = 40
 MODEL_TRAINER_BATCH_SIZE: int = 16
 
